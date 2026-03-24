9a62c570a5a745bfbf9a0e842d5319d881185f2b
selenai9 <s.isimbi2@alustudent.com>
2026-03-24 15:17:12 +0200

    Add localization support for English and Kinyarwanda

--- lib/core/l10n.dart
+++ lib/core/l10n.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HingaL10n {
+  static final Map<String, Map<String, String>> _localizedValues = {
+    'en': {
+      'welcome': 'Welcome to Hinga+',
+      'weather': 'Weather',
+      'pests': 'Pests',
+      'planner': 'Planner',
+      'market': 'Market',
+      'support': 'Support',
+      'sync_status': 'All data synced',
+      'next': 'NEXT',
+      'start': 'START',
+      'skip': 'SKIP',
+    },
+    'rw': {
+      'welcome': 'Murakaza neza kuri Hinga+',
+      'weather': 'HingaHava',
+      'pests': 'HingaShinga',
+      'planner': 'HingaGahunda',
+      'market': 'HingaIsoko',
+      'support': 'HingaInama',
+      'sync_status': 'Byose byageze kuri seriveri',
+      'next': 'KOMEZA',
+      'start': 'TANGIRA',
+      'skip': 'REKA',
+    },
+  };
+
+  static String getString(BuildContext context, String key) {
+    final locale = Localizations.localeOf(context).languageCode;
+    return _localizedValues[locale]?[key] ?? _localizedValues['en']![key]!;
+  }
+}
 
